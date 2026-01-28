f7b820d9599a35341e3ff4430e49da6687c785bd
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-28 17:10:37 +0100

    feat: enhance GetStarted screen with additional text and buttons; add PrimaryButton component

--- app/getStarted.tsx
+++ app/getStarted.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Image } from "expo-image";
 import { SafeAreaViewWrapper } from "@/components/SafeAreaViewWrapper";
 import ThemeButton from "@/components/ThemeButton";
@@ -20,6 +20,18 @@ const GetStarted = () => {
 
       <View className="w-full px-6 ">
         <ProgressBar />
+        <View className='flex flex-col gap-[14px] mt-8'>
+          <Text className='font-dm-medium text-center text-2xl text-neutral-800'>Full contactless experience</Text>
+          <Text className='text-center font-mulish-medium text-base text-neutral-600'>From ordering to paying, that's all contactless</Text>
+        </View>
+        <View className='flex flex-col gap-[10px] mt-8'>
+          <View className='px-6 py-4 w-full'>
+            <Text className='text-center font-mulish-semibold text-base text-purple-3'>Sign up later</Text>
+          </View>
+           <View className='px-6 py-4 w-full'>
+            <Text className='text-center font-mulish-semibold text-base text-purple-3'>Get Started</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaViewWrapper>
   );
---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const PrimaryButton = () => {
+  return (
+    <View className="px-6 py-4 w-full">
+      <Text className="text-center font-mulish-semibold text-base text-purple-3">
+        Sign up later
+      </Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({});
