833fe1a14338b50a3bf55323e7ef0af0e451a069
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-02-15 13:53:46 +0100

    feat: Add dark mode styling to text and button components in the virtual assistant selection screen.

--- app/(protected)/(virtual_assistant)/ChooseVirtualAssistant.tsx
+++ app/(protected)/(virtual_assistant)/ChooseVirtualAssistant.tsx
@@ -17,13 +17,13 @@ const ChooseVirtualAssistant = () => {
         />
       </FadeInView>
       <SlideInUpView delay={200} className="px-6 flex-1">
-        <Text className="font-dm-medium text-[22px] text-neutral-800 text-center">
+        <Text className="font-dm-medium text-[22px] text-neutral-800 dark:text-white text-center">
           Hello! 👋{" "}
         </Text>
-        <Text className="font-dm-medium text-[22px] text-neutral-800 text-center">
+        <Text className="font-dm-medium text-[22px] text-neutral-800 dark:text-white text-center">
           I'm your virtual Assistant
         </Text>
-        <Text className="font-mulish-medium text-neutral-600 text-base text-center mt-[14px]">
+        <Text className="font-mulish-medium text-neutral-600 dark:text-neutral-150 text-base text-center mt-[14px]">
           In order to find the best suited choices for you, please answer the
           next few questions.
         </Text>
@@ -31,7 +31,7 @@ const ChooseVirtualAssistant = () => {
       <SlideInUpView delay={400} className="flex flex-col gap-3 px-6 py-4">
         <PrimaryButton
           text="Take me to the Menu"
-          textClass="text-purple-3"
+          textClass="text-purple-3 dark:text-purple-5"
           onPress={() => {}}
         />
         <PrimaryButton
