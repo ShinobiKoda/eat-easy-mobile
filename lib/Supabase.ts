7a648ed13bcb6f3972784d3adff19b8466866783
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-02-15 12:25:07 +0100

    feat: implement Supabase auth auto-refresh using AppState listener

--- lib/Supabase.ts
+++ lib/Supabase.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 
 const supabaseUrl = "https://xnxfyqqjrtjbjwceozbj.supabase.co";
@@ -13,4 +14,13 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
+});
+
+
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
 });
