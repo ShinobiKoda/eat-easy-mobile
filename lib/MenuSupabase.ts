fc4d73143bd60558e4739fc2678e3f85109c7c7f
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-04-23 13:13:17 +0100

    refactor: remove unused menu item fetching logic and Supabase client configuration

--- lib/MenuSupabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import "react-native-url-polyfill/auto";
-
-const supabaseMenuUrl = process.env.EXPO_PUBLIC_SUPABASE_MENU_URL!;
-const supabaseMenuAnonKey = process.env.EXPO_PUBLIC_SUPABASE_MENU_ANON_KEY!;
-
-export const supabaseMenuItems = createClient(supabaseMenuUrl, supabaseMenuAnonKey);
